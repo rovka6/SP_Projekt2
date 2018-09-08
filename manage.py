c7a3ec722d963044b67e4a31b41c87ca98299426
Igor <iz6968@student.uni-lj.si>
2018-09-08 18:10:22 +0200

    Heroku Config

--- manage.py
+++ manage.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python
 import os
 import sys
+import re
+
+def read_env():
+    try:
+        with open('.env') as f:
+            content = f.read()
+    except IOError:
+        content = ''
+
+    for line in content.splitlines():
+        m1 = re.match(r'\A([A-Za-z_0-9]+)=(.*)\Z', line)
+        if m1:
+            key, val = m1.group(1), m1.group(2)
+            m2 = re.match(r"\A'(.*)'\Z", val)
+            if m2:
+                val = m2.group(1)
+            m3 = re.match(r'\A"(.*)"\Z', val)
+            if m3:
+                val = re.sub(r'\\(.)', r'\1', m3.group(1))
+            os.environ.setdefault(key, val)
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SP_Projekt2.settings")
@@ -19,4 +39,5 @@ if __name__ == "__main__":
                 "forget to activate a virtual environment?"
             )
         raise
+    read_env()
     execute_from_command_line(sys.argv)
